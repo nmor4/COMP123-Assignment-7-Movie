6e631a7b751ee5f49c1fb53f585c67753e4f9dd9
nmor4 <nick-morin@hotmail.com>
2016-08-18 15:04:40 -0400

    Initial commit

---
+++ Assignment 7/Assignment 7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Author: Nick Morin-300654901
+ * Description: COMP123-Assignment 7 - Movie Bonanaza
+ * Date last modified: 2016-08-18
+ */
+
+namespace Assignment_7
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
